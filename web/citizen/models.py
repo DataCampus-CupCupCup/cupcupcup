922703101017a2c7a4f546fae0fb99ff818e1f64
anhyeju <iwillgo555@naver.com>
2020-08-24 18:35:04 +0900

    feat : Make DB Table and draw them into map, Make citizen map

--- web/citizen/models.py
+++ web/citizen/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class PresentCup(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    long = models.FloatField()
+    lat = models.FloatField()
+    address = models.CharField(max_length=255)
+    dirty = models.IntegerField(default=0)
+    
+    # def __str__(self):
+    #     msg= f'{self.id} : 좌표 {self.long} {self.lat}, 주소 {self.address}, 더러움 정도 : {self.dirty}'
+    #     return msg
+
+    class Meta:
+        ordering = ['id']
+    
