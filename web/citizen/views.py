65f728a4d4fd49b9d0006c2538232eb89f8488d1
anhyeju <iwillgo555@naver.com>
2020-08-27 18:40:23 +0900

    feat : Change KAKAO MAP API to Google map API, Draw ploygons, markers on map

--- web/citizen/views.py
+++ web/citizen/views.py
@@ -13,7 +13,8 @@ from .models import PresentCup
 from django.core import serializers
 
 # for csv to DB
-# import pandas as pd
+import pandas as pd
+import json
 
 def index(request):
     return render(request, 'index.html')
@@ -23,15 +24,17 @@ def map(request):
 
 def citizen_map(request, gu='서초구'):
     id_start = 's'
-    
+    df = pd.read_csv('./citizen/real_seocho.csv', encoding='utf-8')
+    df = json.dumps(df.head().to_dict('index'))
+
     qs = PresentCup.objects.filter(id__startswith=id_start)
   
     # javascript 에서 사용하기 위해 json으로 줄것
     json_data = serializers.serialize('json', qs)
+
     # context = { "pc" : json_data }
 
-    context = { "pc" : json_data, 'count' : len(qs)}
-    
+    context = { "pc" : json_data, 'count' : len(qs), 'df': df}
 
     return render(request, 'citizen/citizen_maps.html', context)
     
