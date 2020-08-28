8677b7b7634eaabd5235266babe53adb8d140143
anhyeju <iwillgo555@naver.com>
2020-08-28 18:47:47 +0900

    feat : Chane DB instead of DataFrame, Add listner...'

--- web/citizen/views.py
+++ web/citizen/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 # 모델 불러오기
-from .models import PresentCup
+from .models import PresentCup, PredictCup
 
 # 에러 발생 시
 
@@ -24,17 +24,21 @@ def map(request):
 
 def citizen_map(request, gu='서초구'):
     id_start = 's'
-    df = pd.read_csv('./citizen/real_seocho.csv', encoding='utf-8')
-    df = json.dumps(df.head().to_dict('index'))
-
-    qs = PresentCup.objects.filter(id__startswith=id_start)
-  
+    if gu=='서대문구': id_start = 'sd'
+    elif gu=='강남구' :  id_start = 'gn'
+    
+    # 이 부분을 DB에서 가져오게끔 아래 주석으로 바꾸면 됩니다.
+    # predictCups = pd.read_csv('./citizen/real_seocho.csv', encoding='utf-8')
+    # predictCups_json = json.dumps(df.head().to_dict('index'))
+    predictCups = list(PredictCup.objects.filter(id__startswith=id_start).values())
+    presentCups = list(PresentCup.objects.filter(id__startswith=id_start).values())
+    
+    # print(predictCups.values())
     # javascript 에서 사용하기 위해 json으로 줄것
-    json_data = serializers.serialize('json', qs)
+    predictCups_json = json.dumps(predictCups)
+    presentCups_json = json.dumps(presentCups)
 
-    # context = { "pc" : json_data }
-
-    context = { "pc" : json_data, 'count' : len(qs), 'df': df}
+    context = { "presentCups" : presentCups_json, 'cnt1' : len(presentCups), 'predictCups': predictCups_json, 'cnt2': len(predictCups)}
 
     return render(request, 'citizen/citizen_maps.html', context)
     
@@ -49,6 +53,21 @@ def citizen_map(request, gu='서초구'):
 #     return render(request, 'maps.html')
 
 # def makePresentCup(df):
-#     # ['lat', 'long', 'id', 'dirty', 'address']
-#     pc_instance = PresentCup(id=df.id, long=df.long, lat= df.lat, address=df.address, dirty=df.dirty)
+    # ['lat', 'long', 'id', 'like', 'address', 'dislike']
+    # pc_instance = PresentCup(id=df.id, long=df.long, lat= df.lat, address=df.address, dislike=df.dirty, like=df.like)
+    # pc_instance.save()
+
+
+# 예측
+# def insert(request):
+#     df = pd.read_csv('./citizen/gangnam_predict.csv', encoding='EUC-KR')
+
+#     df.apply(makePredictCup, axis=1)
+
+#     print(PredictCup.objects.all())
+#     return render(request, 'maps.html')
+
+# def makePredictCup(df):
+#     # ['id', lat1', 'long1','lat2', 'long2','lat3', 'long3','lat4', 'long4', 'id', 'dislike','like', 'address']
+#     pc_instance = PredictCup(id=df.id, long1=df.long1, lat1= df.lat1,long2=df.long2, lat2= df.lat2, long3=df.long3, lat3= df.lat3, long4=df.long4, lat4= df.lat4, address=df.address, dislike=df.dislike, like=df.like)
 #     pc_instance.save()
